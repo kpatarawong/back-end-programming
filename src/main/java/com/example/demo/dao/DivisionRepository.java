533d2caec2e085ffc5d3f1b76fc57568b46055f0
Kalvin Saetern <saeternk1@gmail.com>
2025-01-30 00:02:01 -0800

    Complete task E

---
+++ src/main/java/com/example/demo/dao/DivisionRepository.java
@@ -0,0 +1,11 @@
+package com.example.demo.dao;
+
+import com.example.demo.entities.Division;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+
+//repository interfaces for the entities that extend JpaRepository
+@CrossOrigin("http://localhost:4200")
+public interface DivisionRepository extends JpaRepository<Division, Long> {
+}
