76743042beb7ab70ca3508d0f534233deb5d3fda
Kalvin Saetern <saeternk1@gmail.com>
2025-01-30 01:08:53 -0800

    Completed Task H

--- src/main/java/com/example/demo/entities/Country.java
+++ src/main/java/com/example/demo/entities/Country.java
@@ -20,7 +20,7 @@ public class Country {
     @Column(name = "country_id")
     private Long id;
 
-    @Column(name = "countries")
+    @Column(name = "country")
     private String country_name;
 
     @CreationTimestamp
